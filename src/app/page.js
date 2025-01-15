6df6ba068c10a3de1b27871f3a4f20b09b868ea5
Deepansh Kumar <kumardeepansh210@gmail.com>
2025-01-15 19:39:53 +0530

    Created Homepage.

--- src/app/page.js
+++ src/app/page.js
@@ -6,7 +6,7 @@ import React from 'react'
 function page() {
   return (
     <div className='flex items-center justify-center w-full h-full'>
-      <div className="home-svg flex items-center justify-center w-full h-full bg-blue-700">
+      <div className="home-svg flex items-center justify-center w-full h-full ">
   <h1 className="text-9xl sm:text-6xl md:text-7xl delius-regular opacity-40 text-white">
     Ignify
   </h1>

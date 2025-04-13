d6327b43338e4e6103166e45be320be8d3dc720b
Deepansh Kumar <65154092+DeepAnsh210@users.noreply.github.com>
2025-04-13 21:16:47 +0530

    Error alert

--- src/app/today/TaskItem.jsx
+++ src/app/today/TaskItem.jsx
@@ -112,6 +112,7 @@ function TaskItem({ task, onDelete, onUpdate }) {
       onUpdate(response.data.data[0]); // Update the task in the parent component
     } catch (error) {
       console.error(error);
+      alert("Error: " + error.message);
     } finally {
       setIsProcessing(false);
     }
